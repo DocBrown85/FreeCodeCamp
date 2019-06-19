c6455216595bf14c1267ea0a179ab6bb04568f60
docbrown85 <slowhand-85@hotmail.it>
2019-06-19 14:03:28 +0200

    adds roman symbols map

--- 02-javascript-algorithms-and-data-structures/roman-to-numeral-converter/roman-to-numeral-converter.js
+++ 02-javascript-algorithms-and-data-structures/roman-to-numeral-converter/roman-to-numeral-converter.js
@@ -1,3 +1,15 @@
 function convertToRoman(num) {
- return num;
+
+  let romanSymbolsMap = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M'
+  };
+
+  return num;
+
 }

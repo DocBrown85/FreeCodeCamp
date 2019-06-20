41775cff577a0c415e6fbc0691fe5a5d8d1030c8
docbrown85 <slowhand-85@hotmail.it>
2019-06-20 11:45:22 +0200

    use a more straightforward approach to solve the problem.

--- 02-javascript-algorithms-and-data-structures/roman-to-numeral-converter/roman-to-numeral-converter.js
+++ 02-javascript-algorithms-and-data-structures/roman-to-numeral-converter/roman-to-numeral-converter.js
@@ -1,46 +1,16 @@
-function convertToRoman(currentNumber) {
+var convertToRoman = function(num) {
 
-  let getDigitsFromNumeral = function(number) {
+  var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
-    let digits = [];
+  var romanized = '';
 
-    let pow = 0;
-    let currentNumber = number;
-    while (currentNumber > 0) {
-      digit = currentNumber % 10;
-      digits.unshift(digit);
-      pow++;
-      currentNumber = Math.floor(number / Math.pow(10, pow));
+  for (var index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
     }
-
-    return digits;
-
-  };
-
-  let getRomanFromDigits = function(numeralDigits) {
-
-    let romanSymbolsMap = {
-      1: 'I',
-      5: 'V',
-      10: 'X',
-      50: 'L',
-      100: 'C',
-      500: 'D',
-      1000: 'M'
-    };
-
-    return 'XXX';
-
   }
 
-  let numeralDigits = getDigitsFromNumeral(currentNumber);
-  console.log(numeralDigits);
-
-  let roman = getRomanFromDigits(numeralDigits);
-  console.log(roman);
-
-  return roman;
-
+  return romanized;
 }
-
-convertToRoman(12309);

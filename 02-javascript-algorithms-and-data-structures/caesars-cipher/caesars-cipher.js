b1874de4f1dfc87056914256d395f259ea522c16
docbrown85 <slowhand-85@hotmail.it>
2019-06-20 14:02:31 +0200

    adds final solution.

--- 02-javascript-algorithms-and-data-structures/caesars-cipher/caesars-cipher.js
+++ 02-javascript-algorithms-and-data-structures/caesars-cipher/caesars-cipher.js
@@ -1,7 +1,40 @@
 function rot13(str) { // LBH QVQ VG!
 
-  return str;
+  let CaesarCipher = function(_key) {
+
+      let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+      let key = _key;
+
+      this.encode = function(letter) {
+          let encodedLetterIndex = (alphabet.indexOf(letter) + key) % alphabet.length;
+          return alphabet[encodedLetterIndex];
+      };
+
+      this.decode = function(letter) {
+          let decodedLetterIndex = (alphabet.indexOf(letter) - key + alphabet.length) % alphabet.length;
+          return alphabet[decodedLetterIndex];
+      };
+  }
+
+  let key = 13;
+  let caesarCipher = new CaesarCipher(key);
+
+  let characters = str.split("");
+  let decodedCharacters = characters.map((item) => {
+      let isAlphanumericRegex = /[a-z0-9]+$/i;
+      if (isAlphanumericRegex.test(item)) {
+          return caesarCipher.decode(item);
+      }
+      else {
+          return item;
+      }
+  });
+
+  let result = decodedCharacters.join("");
+
+  return result;
 }
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));

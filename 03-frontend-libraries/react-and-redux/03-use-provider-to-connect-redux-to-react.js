e2ca47f13dd258d5ff21f8a980f536e69c3b8617
docbrown85 <slowhand-85@hotmail.it>
2019-07-11 16:35:35 +0200

    completes react-redux tutorial.

--- 03-frontend-libraries/react-and-redux/03-use-provider-to-connect-redux-to-react.js
+++ 03-frontend-libraries/react-and-redux/03-use-provider-to-connect-redux-to-react.js
@@ -1,3 +1,34 @@
+/*
+React and Redux: Use Provider to Connect Redux to React
+In the last challenge, you created a Redux store to handle the messages array
+and created an action for adding new messages. The next step is to provide
+React access to the Redux store and the actions it needs to dispatch updates.
+React Redux provides its react-redux package to help accomplish these tasks.
+
+React Redux provides a small API with two key features: Provider and connect.
+Another challenge covers connect. The Provider is a wrapper component from
+React Redux that wraps your React app. This wrapper then allows you to access
+the Redux store and dispatch functions throughout your component tree. Provider
+takes two props, the Redux store and the child components of your app. Defining
+the Provider for an App component might look like this:
+
+<Provider store={store}>
+<App/>
+</Provider>
+
+The code editor now shows all your Redux and React code from the past several
+challenges. It includes the Redux store, actions, and the DisplayMessages
+component. The only new piece is the AppWrapper component at the bottom. Use
+this top level component to render the Provider from ReactRedux, and pass the
+Redux store as a prop. Then render the DisplayMessages component as a child.
+Once you are finished, you should see your React component rendered to the
+page.
+
+Note: React Redux is available as a global variable here, so you can access the
+Provider with dot notation. The code in the editor takes advantage of this and
+sets it to a constant Provider for you to use in the AppWrapper render method.
+*/
+
 // Redux Code:
 const ADD = 'ADD';
 
@@ -49,22 +80,34 @@ class DisplayMessages extends React.Component {
     });
   }
   render() {
-    return (
-      <div>
-        <h2>Type in a new Message:</h2>
-        <input
-          value={this.state.input}
-          onChange={this.handleChange}/><br/>
-        <button onClick={this.submitMessage}>Submit</button>
-        <ul>
-          {this.state.messages.map( (message, idx) => {
-              return (
-                 <li key={idx}>{message}</li>
-              )
-            })
-          }
-        </ul>
-      </div>
+    return ( <
+      div >
+      <
+      h2 > Type in a new Message: < /h2> <
+      input value = {
+        this.state.input
+      }
+      onChange = {
+        this.handleChange
+      }
+      /><br/ >
+      <
+      button onClick = {
+        this.submitMessage
+      } > Submit < /button> <
+      ul > {
+        this.state.messages.map((message, idx) => {
+          return ( <
+            li key = {
+              idx
+            } > {
+              message
+            } < /li>
+          )
+        })
+      } <
+      /ul> <
+      /div>
     );
   }
 };
@@ -74,11 +117,15 @@ const Provider = ReactRedux.Provider;
 class AppWrapper extends React.Component {
   // render the Provider here
   render() {
-      return (
-        <Provider store={store}>
-            <DisplayMessages />
-        </Provider>
-      );
+    return ( <
+      Provider store = {
+        store
+      } >
+      <
+      DisplayMessages / >
+      <
+      /Provider>
+    );
   }
   // change code above this line
 };

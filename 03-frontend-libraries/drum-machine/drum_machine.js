9aa605d29bbbad1222aadd6afdd863d68e1accd9
docbrown85 <slowhand-85@hotmail.it>
2019-10-01 11:12:24 +0200

    adds sounds banks handling.

--- 03-frontend-libraries/drum-machine/drum_machine.js
+++ 03-frontend-libraries/drum-machine/drum_machine.js
@@ -2,48 +2,183 @@
  * DrumMachine component
  */
 
+const bankOne = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Heater-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Heater-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Heater-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Heater-4',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Clap',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: "Kick-n'-Hat",
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+}, ];
+
+const bankTwo = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Chord-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Chord-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Chord-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Shaker',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: 'Punchy-Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Side-Stick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Snare',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}];
+
 const Display = (props) => {
   return ( <
-    div id = "display" > DISPLAY < /div>
+    div id = "display" > {
+      props.display
+    } < /div>
   )
 }
 
 const DrumPad = (props) => {
   return ( <
     div id = {
-      `drum-pad-${props.padId}`
+      `drum-pad-${props.keyTrigger}`
     }
-    class = 'drum-pad' > {
-      props.padId
-    } < /div>
-  )
+    className = 'drum-pad'
+    onClick = {
+      props.handlePadOnClick
+    } >
+    <
+    audio className = 'clip'
+    src = {
+      props.clip
+    }
+    id = {
+      props.keyTrigger
+    } >
+    <
+    /audio> {
+    props.keyTrigger
+  } < /
+  div >
+)
 }
 
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      display: "display"
+    };
     this.handlePadOnClick = this.handlePadOnClick.bind(this);
   }
 
   handlePadOnClick(e) {
-    this.setState({});
+    console.log(e.target)
+    this.setState({
+      display: e.target.keyTrigger
+    });
+
+  }
+
+  playClip() {
+
   }
 
   render() {
-    let drumPads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'].map((item, index) => {
-      return <DrumPad padId = {
-        item
+    let drumPads = bankOne.map((item, index) => {
+      return <DrumPad keyCode = {
+        item.keyCode
+      }
+      keyTrigger = {
+        item.keyTrigger
+      }
+      clipId = {
+        item.id
+      }
+      clip = {
+        item.url
       }
       key = {
         index
       }
+      handlePadOnClick = {
+        this.handlePadOnClick
+      }
       / >
     });
-    return <div id = "drum-machine" > < Display / > < div id = "pads-container" > {
-      drumPads
-    } < /div > < /div >
-  }
+    return <div id = "drum-machine" > < Display display = {
+      this.state.display
+    }
+    / > < div id = "pads-container" > {
+    drumPads
+  } < /div > < /div >
+}
 }
 
 ReactDOM.render( < DrumMachine / > , document.querySelector('#drum-machine-root'));

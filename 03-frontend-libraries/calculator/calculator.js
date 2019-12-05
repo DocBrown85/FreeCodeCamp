4326eff6ad686a7d29eb351fed1e031437515444
docbrown85 <slowhand-85@hotmail.it>
2019-12-05 16:16:43 +0100

    first commit

---
+++ 03-frontend-libraries/calculator/calculator.js
@@ -0,0 +1,15 @@
+/*
+ * Calculator component
+ */
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+ReactDOM.render(<Calculator />, document.querySelector("#calculator-root"));

f2fefb7e6cc115a3566582c147f8292117fe6364
docbrown85 <slowhand-85@hotmail.it>
2020-01-16 15:47:05 +0100

    refactor colorpleth map building to own function

--- 04-data-visualization/colorpleth-map/chart.js
+++ 04-data-visualization/colorpleth-map/chart.js
@@ -5,9 +5,11 @@ var countyDataURL =
 
 var promises = [d3.json(educationDataURL), d3.json(countyDataURL)];
 
-Promise.all(promises).then(function(data) {
+var buildColorplethMap = function(data) {
   educationData = data[0];
   countyData = data[1];
   console.log(educationData);
   console.log(countyData);
-});
+};
+
+Promise.all(promises).then(buildColorplethMap);

956430961580c0bde8b812ef21c57aeadd83cfad
docbrown85 <slowhand-85@hotmail.it>
2020-01-16 15:11:15 +0100

    add project scaffolding

---
+++ 04-data-visualization/colorpleth-map/chart.js
@@ -0,0 +1,13 @@
+var educationDataURL =
+  "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json";
+var countyDataURL =
+  "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
+
+var promises = [d3.json(educationDataURL), d3.json(countyDataURL)];
+
+Promise.all(promises).then(function(data) {
+  educationData = data[0];
+  countyData = data[1];
+  console.log(educationData);
+  console.log(countyData);
+});

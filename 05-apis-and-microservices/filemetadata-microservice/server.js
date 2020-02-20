64836d96f925dfbedcc434770ed8b5c51c018cac
docbrown85 <slowhand-85@hotmail.it>
2020-02-20 14:07:15 +0100

    remove comments

--- 05-apis-and-microservices/filemetadata-microservice/server.js
+++ 05-apis-and-microservices/filemetadata-microservice/server.js
@@ -12,18 +12,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-/*
-app.use("/public", express.static(process.cwd() + "/public"));
-
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
-
-app.get("/hello", function(req, res) {
-  res.json({greetings: "Hello, API"});
-});
-*/
-
 app.use("/", routes);
 
 app.listen(process.env.PORT || 3000, function() {

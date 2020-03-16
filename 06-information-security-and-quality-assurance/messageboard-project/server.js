5518e464183a0fc65bead2a5afda1c325c237124
docbrown85 <slowhand-85@hotmail.it>
2020-03-16 10:31:18 +0100

    add security policies

--- 06-information-security-and-quality-assurance/messageboard-project/server.js
+++ 06-information-security-and-quality-assurance/messageboard-project/server.js
@@ -21,6 +21,10 @@ app.use(cors({origin: "*"})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(helmet.frameguard({action: "sameorigin"}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({policy: "same-origin"}));
+
 //Sample front-end
 app.route("/b/:board/").get(function(req, res) {
   res.sendFile(process.cwd() + "/views/board.html");

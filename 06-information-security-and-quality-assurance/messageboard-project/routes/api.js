0b11dad3d5f134a741aad2614e17fe06870cb358
Alberto Villa ST <slowhand-85@hotmail.it>
2020-03-17 13:48:44 +0100

    code refactoring

--- 06-information-security-and-quality-assurance/messageboard-project/routes/api.js
+++ 06-information-security-and-quality-assurance/messageboard-project/routes/api.js
@@ -15,9 +15,11 @@ const MessageBoardService = require("../app/message-board-service.js");
 module.exports = function(app) {
   app
     .route("/api/threads/:board")
+
     .get((req, res) => {
       res.status(501).send("not implemented");
     })
+
     .post(
       [
         check("text")
@@ -52,24 +54,30 @@ module.exports = function(app) {
           });
       }
     )
+
     .put((req, res) => {
       res.status(501).send("not implemented");
     })
+
     .delete((req, res) => {
       res.status(501).send("not implemented");
     });
 
   app
     .route("/api/replies/:board")
+
     .get((req, res) => {
       res.status(501).send("not implemented");
     })
+
     .post((req, res) => {
       res.status(501).send("not implemented");
     })
+
     .put((req, res) => {
       res.status(501).send("not implemented");
     })
+
     .delete((req, res) => {
       res.status(501).send("not implemented");
     });

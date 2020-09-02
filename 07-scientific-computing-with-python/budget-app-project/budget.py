01cc78862091ce59697693f35559ce61d03501b7
docbrown85 <slowhand-85@hotmail.it>
2020-09-02 09:39:14 +0200

    add Category class skeleton

--- 07-scientific-computing-with-python/budget-app-project/budget.py
+++ 07-scientific-computing-with-python/budget-app-project/budget.py
@@ -1,5 +1,29 @@
 class Category:
-    pass
+
+    category = None
+    ledger = None
+
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+
+    def __str__(self):
+        pass
+
+    def deposit(self, amount, description=""):
+        pass
+
+    def withdraw(self, amount, description=""):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def transfer(self, amount, a_category):
+        pass
+
+    def check_funds(self, amount):
+        pass
 
 
 def create_spend_chart(categories):

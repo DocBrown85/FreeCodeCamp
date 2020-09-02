5a7b30011d4c97a810d0633dac8dbe512d228723
docbrown85 <slowhand-85@hotmail.it>
2020-09-02 17:04:57 +0200

    add first draft implementation

--- 07-scientific-computing-with-python/budget-app-project/budget.py
+++ 07-scientific-computing-with-python/budget-app-project/budget.py
@@ -8,22 +8,53 @@ class Category:
         self.ledger = []
 
     def __str__(self):
-        pass
+        return ""
 
     def deposit(self, amount, description=""):
-        pass
+        self._add_ledger_item(amount, description)
 
     def withdraw(self, amount, description=""):
-        pass
+        if self.check_funds(amount):
+            amount *= -1
+            self._add_ledger_item(amount, description)
+            return True
+        else:
+            return False
 
     def get_balance(self):
-        pass
+        balance = 0
+        for item in self.ledger:
+            balance += item["amount"]
+
+        return balance
 
     def transfer(self, amount, a_category):
-        pass
+        if self.check_funds(amount):
+            withdraw_description = "Transfer to {}".format(a_category.get_category())
+            self.withdraw(amount, withdraw_description)
+            deposit_description = "Transfer from {}".format(self.get_category())
+            a_category.deposit(amount, deposit_description)
+            return True
+        else:
+            return False
 
     def check_funds(self, amount):
-        pass
+        balance = self.get_balance()
+        if (balance - amount) >= 0:
+            return True
+        else:
+            return False
+
+    def get_category(self):
+        return self.category
+
+    def _add_ledger_item(self, amount, description=""):
+        item = {"amount": None, "description": None}
+
+        item["amount"] = amount
+        item["description"] = description
+
+        self.ledger.append(item)
 
 
 def create_spend_chart(categories):
